1b468745ae1f37cc377fbf932767f71c5b83717c
deftasparagusanaconda <101266955+deftasparagusanaconda@users.noreply.github.com>
2026-08-02 18:17:48 +0530

    more

---
+++ generate categories.py
@@ -0,0 +1,41 @@
+import yaml
+
+the_stuff = yaml.safe_load(open('the stuff.yaml'))
+
+category_names = [next(iter(whatever.keys())) for whatever in iter(the_stuff)]
+header_links = '\n'.join(f'\t\t\t\t<a href="../{category}">{category}</a>' for category in category_names)
+
+def generate_table(category):
+    category_name, entries = next(iter(category.items()))
+    return '\t\t<table>' + ''.join(f'''\t\t\t<tr>
+\t\t\t\t<td><a href="{entry['link']}" target="_blank">{entry['name']}</td>
+\t\t\t\t<td>{entry['desc']}</td>
+\t\t\t</tr>
+''' for entry in entries) + '\t\t</table>'
+
+def generate_html(table): 
+    return f'''<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1"> 
+		<meta name="description" content="daa's map of mathematics">
+		<title>dtgm</title>
+	</head>
+	<body>
+		<header>
+			<h1 style="text-align: center;"><a href="https://github.com/deftasparagusanaconda" target="_blank">daa</a>'s <a href="../">map of mathematics</a></h1>
+			<nav style="text-align: center;">
+{header_links}
+			</nav>
+		</header>
+		<main>
+{table}
+		</main>
+	</body>
+</html>
+'''
+
+for category_name, category in zip(category_names, the_stuff):
+    with open(f'{category_name}/index.html', 'w') as file:
+        file.write(generate_html(generate_table(category)))
